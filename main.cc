60108cc9183c85eeb3660346de5401549eb89fec
evan.teran@gmail.com <evan.teran@gmail.com@d539d033-380d-a057-d972-736f899dd6e9>
2012-04-24 06:05:07 +0000

    made the test app demonstrate both creating an object programatically, and parsing one from a string, and finally extracting a desired value from the parsed object

--- main.cc
+++ main.cc
@@ -12,5 +12,16 @@ int main(int argc, char *argv[]) {
 		.append("test4", json::array().append(1).append(2).append(3).append(4));
 
 	std::cout << json::pretty_print(obj1) << std::endl;
+	
+	
+	json::value v = json::from_string("{\"test3\" : {\"x\" : 123.456},\"test4\" : [1,2,3,{\"z\":12345}],\"test1\" : \"hello world\",\"test2\" : 10}"	);
+	std::cout << json::pretty_print(v) << std::endl;
+	
+	json::value z = v["test4"][3]["z"];
+	
+	std::cout << json::to_number(z) << std::endl;
+	
+
+	
 
 }
